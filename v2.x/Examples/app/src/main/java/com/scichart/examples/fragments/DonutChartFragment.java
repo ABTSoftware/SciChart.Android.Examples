2cf19c4f1c8a1f26d4a16cf54bb1a04558803782
SciChart DevTeam <devteam@scichart.com>
2017-11-15 12:36:06 +0000

    SciChart version 2.1.0.2060

--- v2.x/Examples/app/src/main/java/com/scichart/examples/fragments/DonutChartFragment.java
+++ v2.x/Examples/app/src/main/java/com/scichart/examples/fragments/DonutChartFragment.java
@@ -5,7 +5,7 @@
 // Support: support@scichart.com
 // Sales:   sales@scichart.com
 //
-// PieChartFragment.java is part of the SCICHART® Examples. Permission is hereby granted
+// DonutChartFragment.java is part of the SCICHART® Examples. Permission is hereby granted
 // to modify, create derivative works, distribute and publish any part of this source
 // code whether for commercial, private or personal use.
 //
@@ -27,7 +27,7 @@ import java.util.Collections;
 
 import butterknife.BindView;
 
-public class PieChartFragment extends ExampleBaseFragment {
+public class DonutChartFragment extends ExampleBaseFragment {
     @BindView(R.id.pieChart)
     SciPieChartSurface pieChartSurface;
 
@@ -41,14 +41,16 @@ public class PieChartFragment extends ExampleBaseFragment {
 
     @Override
     protected void initExample() {
-        final IPieRenderableSeries pieSeries = sciChartBuilder.newPieSeries().withSegments(
+        pieChartSurface.setHoleRadius(300);
+
+        final IPieRenderableSeries donutSeries = sciChartBuilder.newDonutSeries().withSegments(
                 sciChartBuilder.newPieSegment().withValue(40).withTitle("Green").withRadialGradientColors(0xff84BC3D, 0xff5B8829).build(),
                 sciChartBuilder.newPieSegment().withValue(10).withTitle("Red").withRadialGradientColors(0xffe04a2f, 0xffB7161B).build(),
                 sciChartBuilder.newPieSegment().withValue(20).withTitle("Blue").withRadialGradientColors(0xff4AB6C1, 0xff2182AD).build(),
                 sciChartBuilder.newPieSegment().withValue(15).withTitle("Yellow").withRadialGradientColors(0xffFFFF00, 0xfffed325).build()
         ).build();
 
-        Collections.addAll(pieChartSurface.getRenderableSeries(), pieSeries);
-        Collections.addAll(pieChartSurface.getChartModifiers(), sciChartBuilder.newLegendModifier(legend).withSourceSeries(pieSeries).build(), new PieSegmentSelectionModifier());
+        Collections.addAll(pieChartSurface.getRenderableSeries(), donutSeries);
+        Collections.addAll(pieChartSurface.getChartModifiers(), sciChartBuilder.newLegendModifier(legend).withSourceSeries(donutSeries).build(), new PieSegmentSelectionModifier());
     }
 }
