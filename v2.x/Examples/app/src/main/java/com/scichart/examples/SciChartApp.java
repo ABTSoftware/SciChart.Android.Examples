735d1fa96b06bfb80a3a935a8c40f291c3d43b58
SciChart DevTeam <devteam@scichart.com>
2019-05-14 11:59:14 +0100

    SciChart version 2.5.0.2580

--- v2.x/Examples/app/src/main/java/com/scichart/examples/SciChartApp.java
+++ v2.x/Examples/app/src/main/java/com/scichart/examples/SciChartApp.java
@@ -65,7 +65,15 @@ public class SciChartApp extends Application {
         // );
 
         try {
-            com.scichart.charting.visuals.SciChartSurface.setRuntimeLicenseKey("");
+            com.scichart.charting.visuals.SciChartSurface.setRuntimeLicenseKey("<LicenseContract>\n" +
+                    "  <Customer>aa</Customer>\n" +
+                    "  <OrderId>bb</OrderId>\n" +
+                    "  <LicenseCount>1</LicenseCount>\n" +
+                    "  <IsTrialLicense>true</IsTrialLicense>\n" +
+                    "  <SupportExpires>04/29/2019 00:00:00</SupportExpires>\n" +
+                    "  <ProductCode>SC-ANDROID-2D-ENTERPRISE-SRC</ProductCode>\n" +
+                    "  <KeyCode>10d505d21df6129e3c83dc9fc45e83532bc5702970e21621044430320ffa1b1659512b28af39a11c4c8064be795b12cf1de3d2e5157e132e37d459dc236c3a0bfe68e0480404370998111056d51f3615a5d23463ccc7180b4cfda4da8837907fcf2a3c7396a6a93e42ce0980029189b284570b4b560c7e4be8cd68f5590e2823d0804d54d922447fc817b08b2f2e223d7d640c27ac1e902c69</KeyCode>\n" +
+                    "</LicenseContract>");
         } catch (Exception e) {
             Log.e("SciChart", "Error when setting the license", e);
         }
